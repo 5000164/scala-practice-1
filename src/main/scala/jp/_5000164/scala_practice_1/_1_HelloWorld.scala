0b668355970164885e13dcb2c175ed12d2fdee5f
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-12-09 23:14:41 +0900

    コードの意図を明確にするため

--- src/main/scala/jp/_5000164/scala_practice_1/_1_HelloWorld.scala
+++ src/main/scala/jp/_5000164/scala_practice_1/_1_HelloWorld.scala
@@ -1,5 +1,6 @@
 package jp._5000164.scala_practice_1
 
 object _1_HelloWorld extends App {
+  // Scala が動作することを確認する
   println("Hello, world!")
 }
--- src/main/scala/jp/_5000164/scala_practice_1/_2_ValVar.scala
+++ src/main/scala/jp/_5000164/scala_practice_1/_2_ValVar.scala
@@ -1,10 +1,12 @@
 package jp._5000164.scala_practice_1
 
 object _2_ValVar extends App {
+  // val は再代入不可
   val valMessage = "Hello, world!"
   // valMessage = "Hello again, world!" // 再代入不可でエラー
   println(valMessage)
 
+  // var は再代入可能
   var varMessage = "Hello, world!"
   varMessage = "Hello again, world!"
   println(varMessage)
