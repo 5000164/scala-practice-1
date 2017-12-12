44d8e90ea01553ff86a8410170d22965afd6ff7b
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-12-12 09:12:11 +0900

    コンパニオンオブジェクトについて学ぶため (途中)

---
+++ src/main/scala/jp/_5000164/scala_practice_1/_6_CompanionObjects.scala
@@ -0,0 +1,39 @@
+package jp._5000164.scala_practice_1
+
+object _6_CompanionObjects extends App {
+  // TODO: コンパニオンオブジェクトについてもっと調べる
+
+  println(SpacerAccumulator.spacing("Hello, world!"))
+  println(SpacerAccumulator.spacing("Hello, world!"))
+}
+
+class SpacerAccumulator {
+  // TODO: アクセスできないことを確認する
+  private var s = ""
+
+  // TODO: 副作用について書く
+  def combine(c: Char, spacer: String): Unit = s += spacer + c
+
+  def combined(): String = s
+}
+
+import scala.collection.mutable
+
+object SpacerAccumulator {
+  // TODO: WeakHashMap がどのような挙動をするのか調べる
+  private val cache = mutable.WeakHashMap.empty[String, String]
+
+  // TODO: 単一の結果式を計算するだけなら中括弧を省略できるということを確認する
+  // TODO: 同じ文字列を渡した場合にキャッシュが使われるか確認する
+  def spacing(s: String): String =
+    if (cache.contains(s))
+      cache(s)
+    else {
+      val accumulator = new SpacerAccumulator
+      for (c <- s)
+        accumulator.combine(c, " ")
+      val result = accumulator.combined()
+      cache += (s -> result)
+      result
+    }
+}
