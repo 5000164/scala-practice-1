c1425ed2063592479d075335b449cf8c124adfbb
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-12-15 08:44:46 +0900

    コンパニオンオブジェクトについての説明を追加

--- src/main/scala/jp/_5000164/scala_practice_1/_6_CompanionObjects.scala
+++ src/main/scala/jp/_5000164/scala_practice_1/_6_CompanionObjects.scala
@@ -1,16 +1,37 @@
 package jp._5000164.scala_practice_1
 
+/**
+  * コンパニオンオブジェクトについて学ぶ
+  *
+  * 渡した文字列を空白区切りにした文字列を返す処理を実装
+  * 一度渡した文字列は再計算せずにキャッシュから取得していることを確認する
+  * コンパニオンオブジェクトはコンパニオンクラスの非公開メンバーにアクセスできることを確認する
+  * コンパニオンオブジェクトではないシングルトンオブジェクトからは非公開メンバーにアクセスできないことを確認する
+  */
 object _6_CompanionObjects extends App {
-  // TODO: コンパニオンオブジェクトについてもっと調べる
+  // Scala では静的メンバーを持てないので、シングルトンオブジェクトを持っている
+  // シングルトンオブジェクトは object キーワードで定義する
+  // クラスと同名のシングルトンオブジェクトをコンパニオンオブジェクトと呼び、同名のクラスをコンパニオンクラスと呼ぶ
+  // コンパニオンクラスとコンパニオンオブジェクトは同一ソースファイル内で定義しなければならない
+  // コンパニオンクラスとコンパニオンオブジェクトは互いの非公開メンバーにアクセスできる
+  // シングルトンオブジェクトはパラメーターをとることができない
+  // シングルトンオブジェクトは初めてアクセスされた時に初期化される
+  // コンパニオンクラスと名前を共有しないシングルトンオブジェクトのことをスタンドアロンオブジェクトと呼ぶ
+  // 継承については後々やる
 
+  // キャッシュがまだなにもないことを確認する
+  // シングルトンオブジェクトなのでこの時に初期化される
   println(SpacerAccumulator.get()) // Map()
 
+  // 一度使用した文字列をキャッシュから取得していることを確認する
   println(SpacerAccumulator.spacing("Hello, world!")) // spaced: H e l l o ,   w o r l d !
   println(SpacerAccumulator.spacing("Hello, world!")) // spaced: H e l l o ,   w o r l d ! by cache
   println(SpacerAccumulator.spacing("Hello again, world!")) // spaced: H e l l o   a g a i n ,   w o r l d !
 
+  // 使用した文字列がキャッシュされているか確認する
   println(SpacerAccumulator.get()) // Map(Hello, world! -> spaced: H e l l o ,   w o r l d !, Hello again, world! -> spaced: H e l l o   a g a i n ,   w o r l d !)
 
+  // コンパニオンオブジェクトではないので非公開メンバーにアクセスできないことを確認する
   Tester.assertNoAccess()
 }
 
@@ -27,7 +48,9 @@ class SpacerAccumulator {
 import scala.collection.mutable
 
 object SpacerAccumulator {
-  // TODO: WeakHashMap がどのような挙動をするのか調べる
+  // 初めてアクセスされた時だけ初期化されることを確認する
+  println("初期化") // 初期化
+
   private val cache = mutable.WeakHashMap.empty[String, String]
 
   // 単一の結果式を計算する場合は中括弧を省略できる
