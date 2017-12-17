3f5cf87799441fa3d29594e8cf4e641073d7e043
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-12-17 23:49:59 +0900

    WeakHashMap の要素が GC で消えるか調べるため

--- src/main/scala/jp/_5000164/scala_practice_1/_7_WeakHashMap.scala
+++ src/main/scala/jp/_5000164/scala_practice_1/_7_WeakHashMap.scala
@@ -4,10 +4,31 @@ import scala.collection.mutable
 import scala.compat.Platform
 
 object _7_WeakHashMap extends App {
-  // TODO: WeakHashMap がどのような挙動をするのか調べる
-
   private val cache = mutable.WeakHashMap.empty[String, String]
+  private val keys = mutable.Map(1 -> "one", 2 -> "two", 3 -> "three")
+
+  println("初期値")
+  println(keys)
   println(cache)
+
+  cache += (keys(1) -> "one")
+  cache += (keys(2) -> "two")
+  cache += (keys(3) -> "three")
+
+  println("")
+  println(keys)
+  println(cache)
+
+  keys -= 1
+
+  println("")
+  println(keys)
+  println(cache)
+
   Platform.collectGarbage
+
+  // TODO: WeakHashMap の要素がなぜ消えないのか調べる
+  println("")
+  println(keys)
   println(cache)
 }
