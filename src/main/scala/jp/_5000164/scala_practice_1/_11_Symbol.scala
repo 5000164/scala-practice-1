7aab5754557a43980cb4dd0e4a7574d3f14dc6cc
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-12-25 20:55:21 +0900

    Symbol について学ぶため

--- src/main/scala/jp/_5000164/scala_practice_1/_11_Symbol.scala
+++ src/main/scala/jp/_5000164/scala_practice_1/_11_Symbol.scala
@@ -2,8 +2,21 @@ package jp._5000164.scala_practice_1
 
 /**
   * シンボルの使い方について学ぶ
+  *
+  * String のインスタンスと Symbol のインスタンスを生成して比較してみる
   */
 object _11_Symbol extends App {
-  val s = 'aSymbol
-  println(s)
+  // String のインスタンスは違うオブジェクトなので同一性の比較では異なる判定になる
+  val string1 = new String("string")
+  val string2 = new String("string")
+  println(string1 eq string2)
+  // false
+
+  // Symbol のインスタンスは同じオブジェクトなので同一性の比較で一致する
+  // シンボルリテラルは先頭に ' をつけて書く
+  // シンボルのコンパニオンオブジェクトを使用してもインスタンスを生成できる
+  val symbol1 = 'symbol
+  val symbol2 = Symbol("symbol")
+  println(symbol1 eq symbol2)
+  // true
 }
